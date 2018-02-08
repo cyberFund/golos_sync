374d587ea827b2a0840d9c521355cbd6e85f7809
Serge Nedashkovsky <sn@cyberfund.io>
2018-02-09 02:27:54 +0300

    Optimized

--- sync_comments.py
+++ sync_comments.py
@@ -1,15 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from pistonapi.steemnoderpc import SteemNodeRPC
-from piston.steem import Post
 from pymongo import MongoClient
 from pprint import pprint
-import collections
 import json
 import time
 import sys
-import os
 from tqdm import tqdm
-# import pistonapi as steemapi
 
 # Golos node params
 rpc = SteemNodeRPC("ws://localhost:8090", apis=["follow", "database"])
@@ -18,34 +14,18 @@ mongo = MongoClient()
 # Database name in MongoDB
 db = mongo.golos_comments
 
-# Calculated last block
-init = db.status.find_one({'_id': 'height'})
-if(init):
-  last_block = init['value']
-else:
-  last_block = 1
-
-# ------------
-# For development:
-#
-# If you're looking for a faster way to sync the data and get started,
-# uncomment this line with a more recent block, and the chain will start
-# to sync from that point onwards. Great for a development environment
-# where you want some data but don't want to sync the entire blockchain.
-# ------------
-
-# last_block = 5298239
 
 def process_op(opObj, block, blockid):
     opType = opObj[0]
     op = opObj[1]
     if opType == "comment":
-        # Update the comment
+        # Update the comment after adding comment
         update_comment(op['author'], op['permlink'])
     if opType == "vote":
-        # Update the comment and vote
+        # Update the comment after voting
         update_comment(op['author'], op['permlink'])
     if opType == "author_reward":
+        # Update the comment after comment reward
         update_comment(op['author'], op['permlink'])
 
 def save_block(block, blockid):
@@ -55,7 +35,6 @@ def save_block(block, blockid):
         '_ts': datetime.strptime(doc['timestamp'], "%Y-%m-%dT%H:%M:%S"),
     })
     db.block.update({'_id': blockid}, doc, upsert=True)
-#    db.block_30d.update({'_id': blockid}, doc, upsert=True)
 
 
 def process_block(block, blockid):
@@ -66,10 +45,6 @@ def process_block(block, blockid):
         process_op(opObj, block, blockid)
     for opObj in ops:
       process_op(opObj['op'], block, blockid)
-
-#    queue_update_account(witness_vote['account'])
-#    if witness_vote['account'] != witness_vote['witness']:
-#        queue_update_account(witness_vote['witness'])
 
 
 def update_comment(author, permlink):
@@ -118,62 +93,59 @@ def update_comment(author, permlink):
                 }
             }
         )
-
-if __name__ == '__main__':
+def sync_comments():
     # Let's find out how often blocks are generated!
     config = rpc.get_config()
     block_interval = config["STEEMIT_BLOCK_INTERVAL"]
+
+    # Last block in the MongoDB
+    init = db.status.find_one({'_id': 'height'})
+    last_block = 1
+    if (init):
+        last_block = init['value']
+    # ------------
+    # For development:
+    #
+    # If you're looking for a faster way to sync the data and get started,
+    # uncomment this line with a more recent block, and the chain will start
+    # to sync from that point onwards. Great for a development environment
+    # where you want some data but don't want to sync the entire blockchain.
+    # ------------
+    # last_block = 13,000,000
+
     # We are going to loop indefinitely
     while True:
 
-        # # -- Process Queue
-        # queue_length = 100
-        # # Don't update automatically if it's older than 3 days (let it update when votes occur)
-        # max_date = datetime.now() + timedelta(-3)
-        # # Don't update if it's been scanned within the six hours
-        # scan_ignore = datetime.now() - timedelta(hours=6)
-        #
-        # # -- Process Queue - Find 100 previous comments to update
-        # queue = db.comment.find({
-        #     'created': {'$gt': max_date},
-        #     'scanned': {'$lt': scan_ignore},
-        # }).sort([('scanned', 1)]).limit(queue_length)
-        # pprint("[Queue] Comments - " + str(queue_length) + " of " + str(queue.count()))
-        # for item in queue:
-        #     update_comment(item['author'], item['permlink'])
-        #
-        # # -- Process Queue - Find 100 comments that have past the last payout and need an update
-        # queue = db.comment.find({
-        #     'cashout_time': {
-        #       '$lt': datetime.now()
-        #     },
-        #     'mode': {
-        #       '$in': ['first_payout', 'second_payout']
-        #     },
-        #     'depth': 0,
-        #     'pending_payout_value': {
-        #       '$gt': 0
-        #     }
-        # }).limit(queue_length)
-        # pprint("[Queue] Past Payouts - " + str(queue_length) + " of " + str(queue.count()))
-        # for item in tqdm(queue, total=queue_length):
-        #     update_comment(item['author'], item['permlink'])
-
-        # Process New Blocks
+        # Last block in the Golos node
         props = rpc.get_dynamic_global_properties()
         block_number = props['last_irreversible_block_num']
+
+        # Message
+        if (block_number - last_block) < 0:
+            print('Synchronization from {:>,} to {:>,} blocks'.format(last_block, block_number))
+        else:
+            print('All blocks are synchronized.')
+            print('Last block number in MongoDB: {:>,} in Golos node: {:>,}.'.format(last_block, block_number))
+
+        # Process New Blocks
+        tqdm_bar = tqdm(total=block_number - last_block + 1)
         while (block_number - last_block) > 0:
             last_block += 1
+            tqdm_bar.update(1)
             # Get full block
             block = rpc.get_block(last_block)
             # Process block
             process_block(block, last_block)
             # Update our block height
-            db.status.update({'_id': 'height'}, {"$set" : {'value': last_block}}, upsert=True)
-            if last_block % 100 == 0:
-                pprint("Processed up to Block #" + str(last_block))
+            db.status.update({'_id': 'height_comments'}, {"$set": {'value': last_block}}, upsert=True)
 
+        tqdm_bar.close()
+        pprint("Processed up to Block #" + str(last_block))
         sys.stdout.flush()
 
         # Sleep for one block
         time.sleep(block_interval)
+
+
+if __name__ == '__main__':
+    sync_comments()
