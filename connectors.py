697501f5d98a6854188e92364e19cdc6cae57c46
Blackmoon <belchikov1996@gmail.com>
2018-02-20 15:22:00 +0300

    Added new blocks

--- connectors.py
+++ connectors.py
@@ -1,34 +1,35 @@
 from pymongo import MongoClient
 
 class Connector:
-  def __init__():
+  def __init__(self):
     pass
 
-  def save_block(block):
+  def save_block(self, block):
     pass
 
-  def find_last_block():
+  def find_last_block(self):
     pass
 
-  def update_last_block(last_block):
+  def update_last_block(self, last_block):
     pass
 
 class MongoConnector(Connector):
-  def __init__(database, host="localhost:27017"):
+  def __init__(self, database, host="localhost:27017"):
     self.client = MongoClient(host)
     self.database = self.client[database]
 
-  def save_block(block):
+  def save_block(self, block):
     collection = block.get_collection()
     dictionary = block.to_dict()
-    self.database[collection].insert(dictionary)    
+    query = block.get_query()
+    self.database[collection].insert(query, dictionary)    
 
-  def find_last_block():
+  def find_last_block(self):
     init = self.database.status.find_one({'_id': 'height_all_tsx'})
     if (init):
       return init['value']
     else: 
       return 1
 
-  def update_last_block(last_block):
+  def update_last_block(self, last_block):
     self.database.status.update({'_id': 'height_all_tsx'}, {"$set": {'value': last_block}}, upsert=True)
