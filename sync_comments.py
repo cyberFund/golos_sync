3d2ee1a44f8881bfd97912406c2d3e8aa4f4f806
Serge Nedashkovsky <sn@cyberfund.io>
2018-02-21 13:33:54 +0300

    Fixed sync_comments

--- sync_comments.py
+++ sync_comments.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from pistonapi.steemnoderpc import SteemNodeRPC
-from pymongo import MongoClient
+from connectors import MongoConnector
 from pprint import pprint
 import json
 import time
 import sys
 from tqdm import tqdm
+from comments import UpdatedComment
 
 # Golos node params
 rpc = SteemNodeRPC("ws://localhost:8090", apis=["follow", "database"])
@@ -15,7 +16,7 @@ def sync_comments():
     comments = connector.get_comments_to_update()
     for comment in comments:
         comment.update(rpc.get_content(comment['author'], comment['permlink']))
-        comment_object = UpdatedComment(comment_content)
+        comment_object = UpdatedComment(comment)
         connector.save_comment(comment_object)
 
 if __name__ == '__main__':
