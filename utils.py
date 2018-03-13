05ce78d38b8e979a283935396320c67335041244
Serge Nedashkovsky <sn@cyberfund.io>
2018-03-14 01:49:20 +0300

    Changed node address

--- utils.py
+++ utils.py
@@ -2,6 +2,6 @@ from connectors import MongoConnector
 from pistonapi.steemnoderpc import SteemNodeRPC
 
 def get_connectors(mongo_database):
-  rpc = SteemNodeRPC("wss://ws.golos.io", apis=["follow", "database"])
+  rpc = SteemNodeRPC("wss://api.golos.cf", apis=["follow", "database"])
   connector = MongoConnector(database=mongo_database)
   return rpc, connector
